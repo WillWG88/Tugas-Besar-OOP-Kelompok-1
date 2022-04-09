5a1b0f5348c3d346801638cde22361d72961ff13
WillWG88 <willgunawan88@gmail.com>
2022-04-09 20:53:19 +0700

    Create Monster.java

---
+++ Monster.java
@@ -0,0 +1,19 @@
+
+
+import com.monstersaku.stats.Stats;
+import com.monstersaku.stats.Buff;
+import com.monstersaku.stats.StatusCondition;
+import com.monstersaku.elementtype.ElementType;
+import com.monstersaku.moves.Move;
+import java.util.List;
+
+public class Monster{
+    private int id;
+    private String nama;
+    private List<ElementType> elementTypes;
+    private Stats baseStats;
+    private Stats stats;
+    private Buff buff; //opsional
+    
+
+}
